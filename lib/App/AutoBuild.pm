d340096d8dafd39c42637c9161f74003844147cb
Joel Jensen <yobert@cpan.org>
2010-12-02 21:03:16 +0000

    Import of YOBERT/App-AutoBuild-0.03 from CPAN.
    gitpan-cpan-distribution: App-AutoBuild
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         YOBERT/App-AutoBuild-0.03.tar.gz
    gitpan-cpan-author:       YOBERT
    gitpan-cpan-maturity:     released

--- lib/App/AutoBuild.pm
+++ lib/App/AutoBuild.pm
@@ -8,11 +8,11 @@ App::AutoBuild - A perl tool to make it quick and easy to compile a C project wi
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
